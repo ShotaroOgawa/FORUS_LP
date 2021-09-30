0d49ad62ec46abc445fcd11df2b111e98f66ef95
{Shotaro} <{0ga0ga.sh0sh0.47@gmail.com}>
2021-09-30 15:59:52 +0900

    21/09/30更新（ほぼ完成）

---
+++ .history/js/script_20210925142937.js
@@ -0,0 +1,19 @@
+$(function() {
+    $(".faq_a").css("display", "none");
+
+    $('.faq_q').click(function(){
+        $(this).next('.faq_a').slideToggle();
+        //②クリックされた.questionの兄弟要素の.answerが開いたり閉じたりする。
+        $('.faq_q').not($(this)).next('.faq_a').slideUp();
+        //③クリックした以外の.questionの兄弟要素の.answerはslideup。
+      });
+
+      $('input:submit[id="btn_submit"]').click(function(){
+		if(!input_check()){
+			return false;
+		}
+	});
+
+
+
+});
---
+++ .history/js/script_20210925143115.js
@@ -0,0 +1,14 @@
+$(function() {
+    $(".faq_a").css("display", "none");
+
+    $('.faq_q').click(function(){
+        $(this).next('.faq_a').slideToggle();
+        //②クリックされた.questionの兄弟要素の.answerが開いたり閉じたりする。
+        $('.faq_q').not($(this)).next('.faq_a').slideUp();
+        //③クリックした以外の.questionの兄弟要素の.answerはslideup。
+      });
+
+});
+
+
+// FAQ-----------
